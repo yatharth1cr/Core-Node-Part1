33e7dfde9526eb77fad1f4ddf8d7bef02b3a8ac4
yatharth1cr <yatharthgiri187@gmail.com>
2024-04-29 10:04:00 +0530

    checked REPL in terminal

---
+++ Introduction-Node.js/ExpainWithCode/index.js
@@ -0,0 +1,24 @@
+console.log("Welcome to Node.js");
+
+function square(num) {
+  return num * num;
+}
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+console.log(square(12));
+// $ node index.js
+// Welcome to Node.js
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+// 144
